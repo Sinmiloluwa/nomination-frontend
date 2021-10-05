be5787f28c20afd83ed53150add07027ffd9affd
Sinmiloluwa <mofeoluwae@gmail.com>
2021-10-05 00:15:56 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+// Nominate
+function Nominate() {
+  const fullName = document.getElementById('fullname').value;
+  const mail = document.getElementById('email').value;
+  const linkedIn = document.getElementById('linkedin').value;
+  const twitter = document.getElementById('twitter').value;
+  const category = document.getElementById('option').value;
+
+  let data = {
+    fullname: fullName,
+    email: mail,
+    linkedin: linkedIn,
+    twitter: twitter,
+    category: category
+  }
+
+  const url = 'http://127.0.0.1:8000/api/add';
+
+  axios.post(url,data)
+  
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+  console.error(error.response.data);
+  })
+
+  location.reload();  
+}
+
+// get categories
+function getCategories() {
+  fetch('http://localhost:8000/api/categories') 
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(data) {
+    for (let i=0; i < data.length; i++) {
+       var option = "<option value='" + data[i].name + "'>" + data[i].name + "</option>" 
+       document.getElementById('option').innerHTML += option 
+       console.log(data[i].name)
+     }
+  })
+}
+
+// Accept Nomination
+function acceptNomination() {
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const alt = document.getElementById('alt').value;
+    const linkedIn = document.getElementById('linkedin').value;
+    const twitter = document.getElementById('twitter').value;
+    const insta = document.getElementById('instagram').value;
+
+    let data = {
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+      phone_number: phone,
+      alt: alt,
+      linkedin: linkedIn,
+      twitter: twitter,
+      instagram: insta
+    }
+
+    const url = "http://localhost:8000/api/nominee/create"
+
+    axios.post(url,data)
+  
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+  console.error(error.response.data);
+  })
+
+  window.location.replace("index.html")
+}
+
+
+
+
+
+   
